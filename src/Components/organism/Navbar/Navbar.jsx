4e4e1a5f6195527c1e53e34d4540378aa6173558
aayush <ayushwasnik999@gmail.com>
2023-07-01 03:09:34 +0530

    added homepage and about page

---
+++ src/Components/organism/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styles from "./Navbar.module.css";
+import logo from "../../../assests/logo.png";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className={styles.navbar}>
+        <div className={styles.homeLogo}>
+          <a href="#">
+            <img src={logo} alt="Gym Logo" />
+          </a>
+        </div>
+        <ul className={styles.links}>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#program">Program</a>
+          </li>
+          <li>
+            <a href="#training">Training</a>
+          </li>
+          <li>
+            <a href="#pricing">Pricing</a>
+          </li>
+        </ul>
+        <button className={styles.btn}>Join Us</button>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
